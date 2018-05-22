ea16a2f6a9b8c8662f90ca9602adb0f14ae173c2
OmegaNix <N.T.KRIEGER@GMAIL.COM>
2018-05-21 22:28:08 -0400

    improved workflow, still debugging

--- scripts.js
+++ scripts.js
@@ -9,7 +9,8 @@
 			application_ID: "275f81d1fd86d9496a2bfccdf046677f1b6df0915b60c9058f9b4c521aae9223",
 			secret: "75c4f0665e1d36c59ba4b03cecc6207212e1579777bc423e9300b29d9ec18038",
 			callbackURL: "urn:ietf:wg:oauth:2.0:oob",
-			public_URL: "https://api.unsplash.com/photos/random/?client_id="	
+			public_URL: "https://api.unsplash.com/photos/random/?client_id=",
+			imageURL: ""
 		};		
 		
 		const Text = {
@@ -32,16 +33,34 @@
 				success: function(data, status){
 					return data.urls.raw;								
 				}
-			}); 	
+			}); 
+					
 		}
-		
+
 		//returns a randomly generated sentence using markov chains derived from Naked Lunch
 		function getRandomSentence(){
 			return Text.markovLunch.generateSentences(1);
 		} 
 		
-		//mg button
-		$("#mg_button").click(function(){
+		//loads a random image into a MemeGenerator template
+		function createGenerator(){
+			$.ajax
+			({
+				//beforeSend: function(xhr){xhr.setRequestHeader('apiKey', MG.apiKey)},
+				url: MG.url + "//Generator_Create",
+				apiKey: MG.apiKey,
+				type: "GET",
+				image: getRandomPhotoUrl(),
+				displayName: "naked lunch (remix)" + Date.now(),
+
+				success: function(data, status){
+					
+					alert("Data: " + data + "\nStatus: " + status);					
+				}
+			}); 	
+		}
+		//creates the final captioned image
+		function createInstance(){
 			$.ajax
 			({
 				//beforeSend: function(xhr){xhr.setRequestHeader('apiKey', MG.apiKey)},
@@ -56,10 +75,12 @@
 				}
 
 			}); 	
-		});
-		
-	
-				
+		}
+
+		$("#test_button").click(function(){
+			//createGenerator();
+			$("#test").text(getRandomPhotoUrl());
+		});				
 	});
 })();
 
