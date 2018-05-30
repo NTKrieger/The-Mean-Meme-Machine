9aef0ca8e90ae7ca95527e9d44d7d2a8f88d6f1f
OmegaNix <N.T.KRIEGER@GMAIL.COM>
2018-05-29 21:03:15 -0400

    getTopicalPhotoURL() returns HTTP: 200.  Need to write code oto parse response

--- scripts.js
+++ scripts.js
@@ -7,10 +7,9 @@
 		//all constants by namespace		
 		let Unsplash = {
 			application_ID: "275f81d1fd86d9496a2bfccdf046677f1b6df0915b60c9058f9b4c521aae9223",
-			secret: "75c4f0665e1d36c59ba4b03cecc6207212e1579777bc423e9300b29d9ec18038",
-			callbackURL: "urn:ietf:wg:oauth:2.0:oob",
+			token: 'e2b8461a399ab1bea1fa96b8744a3c622f621655e2ede44bbdc05b356ace2641',
 			random_URL: "https://api.unsplash.com/photos/random/?client_id=",
-			search_URL: "https://api.unsplash.com/search/photos/?client_id=",
+			search_URL: "https://api.unsplash.com/search/photos/?query="
 		};		
 		
 		let Text = {
@@ -40,10 +39,11 @@
 		function getTopicalPhotoUrl(search_term){
 			$.ajax
 			({
-				url:  Unsplash.search_URL + Unsplash.application_ID,
-				type: "GET",
-				query: "test",
-								
+				beforeSend: function (xhr) {
+					xhr.setRequestHeader ("Authorization", "Bearer " + Unsplash.token);
+				},
+				url:  Unsplash.search_URL + "search_term",
+				type: "GET",								
 				success: function(data, status){
 					alert(data,status);								
 				}
